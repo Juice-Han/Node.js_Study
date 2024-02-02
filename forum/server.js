643fdf37f49901e405fe89da233c57b785f76a43
JuHan Park <iiiamst16@gmail.com>
2024-02-02 20:04:37 +0900

    connect MongoDB & make a document

--- forum/server.js
+++ forum/server.js
@@ -3,16 +3,28 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080, () => {
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://admin:admin1234@cluster0.mpunoid.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(8080, () => {
     console.log('http://localhost:8080에서 서버 실행 중');
 })
+}).catch((err)=>{
+  console.log(err)
+})
+
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
 app.get('/news', (request, response) => {
-    response.send('뉴스페이지');
+    db.collection('post').insertOne({title: 'first'});
+    // response.send('뉴스페이지');
 })
 
 app.get('/users', (request, response) => {
