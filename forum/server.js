5a49a7cdb65303c393ed8ed06540363e188f5e60
juhanPark <iiiamst16@gmail.com>
2024-02-05 16:55:36 +0900

    글 발행기능 구현

--- forum/server.js
+++ forum/server.js
@@ -2,19 +2,22 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const { MongoClient } = require('mongodb')
 
 let db
-const url = 'mongodb+srv://admin:admin1234@cluster0.mpunoid.mongodb.net/?retryWrites=true&w=majority'
-new MongoClient(url).connect().then((client)=>{
-  console.log('DB연결성공')
-  db = client.db('forum')
-  app.listen(8080, () => {
-    console.log('http://localhost:8080에서 서버 실행 중');
-})
-}).catch((err)=>{
-  console.log(err)
+const url = 'mongodb+srv://admin:admin1234@cluster0.uyhgcoc.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결성공')
+    db = client.db('forum')
+    app.listen(8080, () => {
+        console.log('http://localhost:8080에서 서버 실행 중');
+    })
+}).catch((err) => {
+    console.log(err)
 })
 
 
@@ -22,11 +25,35 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/news', (request, response) => {
-    db.collection('post').insertOne({title: 'first'});
-    // response.send('뉴스페이지');
-})
-
 app.get('/users', (request, response) => {
     response.sendFile(__dirname + '/about.html');
+})
+
+app.get('/list', async (request, response) => {
+    let result = await db.collection('post').find().toArray()
+    response.render('list.ejs', { posts: result })
+})
+
+app.get('/time', (request, response) => {
+    let time = new Date()
+    response.render('time.ejs', { time: time })
+})
+
+app.get('/write', (request, response) => {
+    response.render('write.ejs')
+})
+
+app.post('/add', async (request, response) => {
+    try{
+        if (request.body.title === '') {
+            response.send('제목을 입력해주세요')
+        } else {
+            await db.collection('post').insertOne({ title: request.body.title, content: request.body.content })
+            response.redirect('/list')
+        }
+    }catch(e){
+        console.log(e)
+        response.status(500).send('서버에러')
+    }
+    
 })
