5986a83f79daf98093dbed88e3d7802d20020e8c
juhanPark <iiiamst16@gmail.com>
2024-02-06 21:35:41 +0900

    init

---
+++ my-blog-app/server/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+
+app.listen(8080, ()=>{
+    console.log('8080 포트에서 서버 실행중')
+})
+
+app.get('/',(req,res) => {
+    res.send('Hello World!')
+})
