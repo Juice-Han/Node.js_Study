82690ac0a15b96710c3fb0905f09d8588cbea06e
juhanPark <iiiamst16@gmail.com>
2024-02-07 10:47:17 +0900

    tailwind 삭제 & styled-components 적용

--- my-blog-app/src/components/Header.tsx
+++ my-blog-app/src/components/Header.tsx
@@ -1,21 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  width: 100%;
+  height: 40px;
+  background-color: gray;
+`
 
 function Header() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <div>
-      <div className="w-full h-16 bg-slate-900 flex items-center">
-        <div className="w-44 h-9 text-2xl text-white ml-3">
-          <span onClick={()=>{navigate('/')}}>Juice-Han Blog</span>
-        </div>
-        <div className="text-lg text-slate-200 ml-2">
-          <span onClick={()=>{navigate('/write')}}>글쓰기</span>
-        </div>
-        <div className="text-lg text-slate-200 ml-2">
-          <span onClick={()=>{navigate('/posts')}}>글목록</span>
-        </div>
-      </div>
-    </div>
+    <Container>
+      
+    </Container>
   );
 }
 
--- my-blog-app/src/components/Write.tsx
+++ my-blog-app/src/components/Write.tsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+
 export default function Write() {
-  return <div>글쓰기 페이지</div>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <div>
+      <div>
+        <h4>글 제목</h4>
+        <input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <p>글 내용</p>
+        <input
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        />
+        <button>저장하기</button>
+      </div>
+    </div>
+  );
 }
