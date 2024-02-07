7646eebfac7af90a7d19544e821344e2c6a9efae
juhanPark <iiiamst16@gmail.com>
2024-02-07 10:58:23 +0900

    Header 디자인

--- my-blog-app/src/components/Header.tsx
+++ my-blog-app/src/components/Header.tsx
@@ -4,15 +4,37 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
-  height: 40px;
-  background-color: gray;
+  height: 60px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+`
+
+const LogoTitle = styled.div`
+  width: 180px;
+  height: 30px;
+  font-size: 24px;
+  font-weight: 600;
+  color: white;
+  text-align: center;
+  line-height: 1.1;
+`
+
+const NavItem = styled.div`
+  width: 60px;
+  height: 25px;
+  font-size: 16px;
+  color: white;
+  margin-left: 10px;
 `
 
 function Header() {
   const navigate = useNavigate();
   return (
     <Container>
-      
+      <LogoTitle onClick={()=>{navigate('/')}}>JuiceHan Blog</LogoTitle>
+      <NavItem onClick={()=>{navigate('/write')}}>글 쓰기</NavItem>
+      <NavItem onClick={()=>{navigate('/posts')}}>글 목록</NavItem>
     </Container>
   );
 }
