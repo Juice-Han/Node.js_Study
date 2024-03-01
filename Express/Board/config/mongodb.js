ad14bcf11d07edbfe8772a962cf05b957dfe3659
juhanPark <iiiamst16@gmail.com>
2024-03-01 10:27:55 +0900

    connect-mongo 연동해서 session정보 mongodb에 저장

--- Express/Board/config/mongodb.js
+++ Express/Board/config/mongodb.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 
 const client = new MongoClient(`mongodb+srv://${process.env.MONGODB_LOGIN}@cluster0.uyhgcoc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 
-async function connectDB(listen){
+async function connectDB(callback){
     try{
         await client.connect();
         console.log('mongoDB 연결')
         db = client.db("board");
-        listen();
+        callback();
     }catch(err){
         console.error(err);
     }
--- Express/Board/server.js
+++ Express/Board/server.js
@@ -1,29 +1,37 @@
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const boardRouter = require('./router/board.js');
 const userRouter = require('./router/user.js');
-const { connectDB } = require('./config/mongodb.js');
+const { connectDB, client } = require('./config/mongodb.js');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
         httpOnly: true,
-        secure: false
-    }
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    },
+    store: MongoStore.create({
+        client,
+        dbName: 'board-session'
+    })
 }))
 app.use(cookieParser());
-connectDB(()=>{app.listen(8080, ()=>{
-    console.log('8080 포트에서 서버 실행중');
-})})
+
+connectDB(() => {
+    app.listen(8080, () => {
+        console.log('8080 포트에서 서버 실행중');
+    })
+})
 
 app.use('/board', boardRouter);
 app.use('/user', userRouter);
