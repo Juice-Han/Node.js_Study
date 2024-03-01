d945f38f7b94116e6cb97586fe7acedb7b1810ef
juhanPark <iiiamst16@gmail.com>
2024-03-01 12:05:59 +0900

    res.json 하단에 있던 return 문 앞으로 수정

--- Express/Board/router/board.js
+++ Express/Board/router/board.js
@@ -7,60 +7,54 @@ const db = client.db('board');
 
 router.get('/posts', async (req, res) => {
     const posts = await db.collection('posts').find().toArray();
-    res.status(200).json({ posts: posts, message: '정상적으로 데이터를 불러왔습니다.' });
+    return res.status(200).json({ posts: posts, message: '정상적으로 데이터를 불러왔습니다.' });
 })
 
 router.post('/posts', async (req, res) => {
-    if(!req.session.is_login){
-        res.status(401).send({message: '로그인을 한 후 이용해주세요.'});
-        return;
+    if (!req.session.is_login) {
+        return res.status(401).send({ message: '로그인을 한 후 이용해주세요.' });
     }
     const post = req.body;
     if (!post.title || !post.content) {
-        res.status(400).json({ message: "필수 입력 사항을 입력하세요." });
-        return;
+        return res.status(400).json({ message: "필수 입력 사항을 입력하세요." });
     }
     const result = await db.collection('posts').insertOne({ title: post.title, content: post.content });
     if (result.acknowledged === false) {
-        res.status(500).json({ message: '데이터베이스에 글이 저장되지 않았습니다.' });
-        return;
+        return res.status(500).json({ message: '데이터베이스에 글이 저장되지 않았습니다.' });
     }
-    res.status(201).json({ message: '정상적으로 글이 작성되었습니다.' });
+    return res.status(201).json({ message: '정상적으로 글이 작성되었습니다.' });
 })
 
 router.get('/posts/:id', async (req, res) => {
     const filter = { _id: new ObjectId(req.params.id) };
     const post = await db.collection('posts').findOne(filter);
     if (!post) {
-        res.status(400).json({ message: '해당하는 데이터가 존재하지 않습니다.' });
-        return;
+        return res.status(400).json({ message: '해당하는 데이터가 존재하지 않습니다.' });
     }
-    res.status(200).json({ post: post, message: '정상적으로 데이터를 불러왔습니다.' });
+    return res.status(200).json({ post: post, message: '정상적으로 데이터를 불러왔습니다.' });
 })
 
 router.patch('/posts/:id', async (req, res) => {
     const query = { _id: new ObjectId(req.params.id) };
     if (!req.body.title || !req.body.content) {
-        res.status(400).json({ message: "필수 입력 사항을 입력하세요." });
-        return;
+        return res.status(400).json({ message: "필수 입력 사항을 입력하세요." });
     }
     const updateDoc = { $set: { title: req.body.title, content: req.body.content } };
     const result = await db.collection('posts').updateOne(query, updateDoc);
     if (result.modifiedCount !== 1) {
-        res.status(500).json({ message: '데이터베이스에 변경사항이 저장되지 않았습니다.' });
-        return;
+        return res.status(500).json({ message: '데이터베이스에 변경사항이 저장되지 않았습니다.' });
     }
-    res.status(200).json({ message: '정상적으로 수정되었습니다.' });
+    return res.status(200).json({ message: '정상적으로 수정되었습니다.' });
 })
 
 router.delete('/posts/:id', async (req, res) => {
     const query = { _id: new ObjectId(req.params.id) };
     const result = await db.collection('posts').deleteOne(query);
     if (result.deletedCount !== 1) {
-        res.status(400).json({ message: '해당하는 데이터가 존재하지 않습니다.' });
-        return;
+        return res.status(400).json({ message: '해당하는 데이터가 존재하지 않습니다.' });
+
     }
-    res.status(200).json({ message: '데이터가 정상적으로 삭제되었습니다.' });
+    return res.status(200).json({ message: '데이터가 정상적으로 삭제되었습니다.' });
 })
 
 module.exports = router
--- Express/Board/router/user.js
+++ Express/Board/router/user.js
@@ -5,42 +5,36 @@ const collection = client.db('board').collection('users')
 
 router.post('/register', async (req, res) => {
     if (!req.body.id || !req.body.password) {
-        res.json({ message: '필수 입력사항을 입력해주세요.' });
-        return;
+        return res.json({ message: '필수 입력사항을 입력해주세요.' });
     }
     const user = await collection.findOne({ id: req.body.id });
     if (user) {
-        res.status(400).json({ message: '이미 존재하는 id입니다.' });
-        return;
+        return res.status(400).json({ message: '이미 존재하는 id입니다.' });
     }
     const result = await collection.insertOne(req.body);
     if (!result) {
-        res.status(500).json({ message: '계정을 등록하지 못했습니다.' });
-        return;
+        return res.status(500).json({ message: '계정을 등록하지 못했습니다.' });
     }
-    res.status(201).json({ message: '계정이 생성되었습니다.' });
+    return res.status(201).json({ message: '계정이 생성되었습니다.' });
 })
 
 router.post('/login', async (req, res) => {
     if (!req.body.id || !req.body.password) {
-        res.json({ message: '필수 입력사항을 입력해주세요.' });
-        return;
+        return res.json({ message: '필수 입력사항을 입력해주세요.' });
     }
     const user = await collection.findOne({ id: req.body.id });
     if (!user) {
-        res.status(400).json({ message: '존재하지 않는 아이디입니다.' });
-        return;
+        return res.status(400).json({ message: '존재하지 않는 아이디입니다.' });
     }
     if (req.body.password !== user.password) {
-        res.status(400).json({ message: '비밀번호를 다시 입력해주세요.' });
-        return;
+        return res.status(400).json({ message: '비밀번호를 다시 입력해주세요.' });
     }
     req.session.is_login = true;
-    res.status(200).json({ message: '로그인 성공' });
+    return res.status(200).json({ message: '로그인 성공' });
 })
 
 router.post('/logout', async (req, res) => {
-    if(!req.session.is_login){
+    if (!req.session.is_login) {
         return res.json({ message: '올바른 접근 방식이 아닙니다.' });
     }
     req.session.destroy();
