8b13ce2c987dd153179ab6c8728e1896aa5fe5b5
JuHan Park <iiiamst16@gmail.com>
2024-03-04 22:43:16 +0900

    bycript를 사용한 비밀번호 암호화 후 db에 저장

--- Express/Board/router/user.js
+++ Express/Board/router/user.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const { client } = require('../config/mongodb.js');
 
 const collection = client.db('board').collection('users')
@@ -11,7 +12,9 @@ router.post('/register', async (req, res) => {
     if (user) {
         return res.status(400).json({ message: '이미 존재하는 id입니다.' });
     }
-    const result = await collection.insertOne(req.body);
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const result = await collection.insertOne({id: req.body.id, password: hash});
     if (!result) {
         return res.status(500).json({ message: '계정을 등록하지 못했습니다.' });
     }
@@ -19,8 +22,8 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-    if(req.session.is_login){
-        return res.status(400).json({message: '정상적인 접근이 아닙니다.'})
+    if (req.session.is_login) {
+        return res.status(400).json({ message: '정상적인 접근이 아닙니다.' })
     }
     if (!req.body.id || !req.body.password) {
         return res.status(400).json({ message: '필수 입력사항을 입력해주세요.' });
@@ -29,7 +32,8 @@ router.post('/login', async (req, res) => {
     if (!user) {
         return res.status(400).json({ message: '존재하지 않는 아이디입니다.' });
     }
-    if (req.body.password !== user.password) {
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordCorrect) {
         return res.status(400).json({ message: '비밀번호를 다시 입력해주세요.' });
     }
     req.session.is_login = true;
