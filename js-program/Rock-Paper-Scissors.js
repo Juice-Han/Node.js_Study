8c810b541d1037d72d80cbc66441a4c0e0f49b4d
JuHan Park <iiiamst16@gmail.com>
2024-03-16 23:17:11 +0900

    필요없는 주석 삭제

--- js-program/Rock-Paper-Scissors.js
+++ js-program/Rock-Paper-Scissors.js
@@ -1,15 +1,5 @@
 const readline = require("readline");
 
-
-
-/* // 생성한 rl 변수를 사용하는 법
-rl.on("line", (line) => {
-    // 한 줄씩 입력받은 후 실행할 코드
-    rl.close(); // close가 없으면 입력을 무한히 받는다.
-});
-rl.on('close', () => {
-    // 입력이 끝난 후 실행할 코드
-}) */
 function 가위바위보() {
     // 1 : 가위, 2 : 바위, 3 : 보 의미
     const opponent = Math.floor(Math.random() * 3 + 1);
