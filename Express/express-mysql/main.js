0f0ea7608b5680c6cc74f77ee87b11791cde6896
JuHan Park <iiiamst16@gmail.com>
2024-03-18 22:18:33 +0900

    글 생성성할 때 저자 선택 기능 추가

--- Express/express-mysql/main.js
+++ Express/express-mysql/main.js
@@ -38,7 +38,7 @@ var app = http.createServer(function (request, response) {
         if (error) {
           throw error;
         }
-        db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function (error2, topic) {
+        db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`, [queryData.id], function (error2, topic) {
           if (error2) {
             throw error2;
           }
@@ -46,7 +46,10 @@ var app = http.createServer(function (request, response) {
           var description = topic[0].description;
           var list = template.list(topics);
           var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
+            `<h2>${title}</h2>
+            ${description}
+            <p>by ${topic[0].name}</p>
+            `,
             ` <a href="/create">create</a>
                 <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
@@ -61,24 +64,29 @@ var app = http.createServer(function (request, response) {
     }
   } else if (pathname === '/create') {
     db.query(`SELECT * FROM topic`, function (error, topics) {
-      var title = 'Create';
-      var list = template.list(topics);
-      var html = template.HTML(title, list,
-        `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `,
-        `<a href="/create">create</a>`
-      );
-      response.writeHead(200);
-      response.end(html);
+      db.query(`SELECT * FROM author`, function (error2, authors) {
+        var title = 'Create';
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+          `
+            <form action="/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                ${template.authorSelect(authors)}
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `,
+          `<a href="/create">create</a>`
+        );
+        response.writeHead(200);
+        response.end(html);
+      })
     })
   } else if (pathname === '/create_process') {
     var body = '';
@@ -88,7 +96,7 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       db.query(`INSERT INTO topic (title, description, created, author_id) VALUES (?, ?, NOW(), ?)`, [
-        post.title, post.description, 1
+        post.title, post.description, post.author
       ], function (error, results) {
         if (error) {
           throw error;
@@ -98,27 +106,6 @@ var app = http.createServer(function (request, response) {
       })
     });
   } else if (pathname === '/update') {
-    /* db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function (error, topics) {
-      var title = 'Update';
-      var list = template.list(topics);
-      var html = template.HTML(title, list,
-        `
-          <form action="/update_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <input type="hidden" name="id" value="${queryData.id}">
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-        `,
-        `<a href="/create">create</a>`
-      );
-      response.writeHead(200);
-      response.end(html);
-    }) */
     db.query(`SELECT * FROM topic`, function (error, topics) {
       if (error) {
         throw error;
@@ -169,8 +156,8 @@ var app = http.createServer(function (request, response) {
     });
     request.on('end', function () {
       var post = qs.parse(body);
-      db.query(`DELETE FROM topic WHERE id=?`,[post.id],function(error, results){
-        if(error){
+      db.query(`DELETE FROM topic WHERE id=?`, [post.id], function (error, results) {
+        if (error) {
           throw error;
         }
         response.writeHead(302, { Location: `/` });
