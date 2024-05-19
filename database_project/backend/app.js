3d6112498964bcc3040df92b931e936ba373d35b
juhanPark <iiiamst16@gmail.com>
2024-05-19 12:03:19 +0900

    문제 관리 기능 구현

--- database_project/backend/app.js
+++ database_project/backend/app.js
@@ -63,3 +63,47 @@ app.delete('/user/:user_id', (req,res)=>{
         return res.json({message: '삭제 성공'})
     })
 })
+
+app.get('/problem', (req,res)=>{
+    db.query('select * from problem', (err, rows)=>{
+        if(err){
+            console.error(err)
+            return res.json({message: '서버 오류'})
+        }
+        return res.json({rows})
+    })
+})
+
+app.post('/problem', (req,res)=>{
+    const {title, description, difficulty} = req.body
+    db.query('insert into problem (title, description, difficulty) values(?, ?, ?)',[title,description,difficulty], (err,result) => {
+        if(err){
+            console.error(err)
+            return res.json({message: '서버 오류'})
+        }
+        return res.json({message: '삽입 성공'})
+    })
+})
+
+app.put('/problem/:problem_num', (req,res)=>{
+    const problem_num= req.params.problem_num
+    const {title, description, difficulty} = req.body
+    db.query('update problem set title=?, description=?, difficulty=? where problem_num=?',[title,description,difficulty,problem_num],(err,result)=>{
+        if(err){
+            console.error(err)
+            return res.json({message: '서버 오류'})
+        }
+        return res.json({message: '수정 성공'})
+    })
+})
+
+app.delete('/problem/:problem_num', (req,res)=>{
+    const problem_num= req.params.problem_num
+    db.query('delete from problem where problem_num=?',problem_num,(err,result)=>{
+        if(err){
+            console.error(err)
+            return res.json({message: '서버 오류'})
+        }
+        return res.json({message: '삭제 성공'})
+    })
+})
